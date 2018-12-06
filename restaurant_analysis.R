52de30bff42866a7efbd82bd9d42d8671e728592
Frankie Tam <frankie@Frankies-MBP.fios-router.home>
2018-12-05 23:29:28 -0500

    Complete Part D and E

--- restaurant_analysis.R
+++ restaurant_analysis.R
@@ -5,7 +5,6 @@
 
 source('library.R')
 
-
 all_data <- read.csv('DOHMH_New_York_City_Restaurant_Inspection_Results.csv')
 
 #Part A(a.)
@@ -32,7 +31,7 @@ all_data$INSPECTION.DATE <- NULL
 
 #(iii.)
 #Rename columns
-all_data <- rename(all_data, c('CAMIS' = 'id',
+all_data <- plyr::rename(all_data, c('CAMIS' = 'id',
                                'BORO' = 'borough',
                                'CUISINE.DESCRIPTION' = 'cuisine',
                                'ACTION' = 'action', 
@@ -57,7 +56,7 @@ all_data$action <- gsub(pattern = ' s', replacement = '', x = all_data$action)
 all_data$action <- gsub(pattern = ' and', replacement = '', x = all_data$action)
 
 
-#all_data <- all_data %>% filter(action != '')
+all_data <- all_data %>% filter(action != '')
 
 
 #Part A(b.)
@@ -69,8 +68,8 @@ all_data <- all_data %>% filter(all_data$borough != "Missing")
 all_data <- all_data %>%  filter(all_data$inspection_date != "1900-01-01")
 
 #Remove observations without or with negative score 
-#all_data <- all_data %>%  filter(all_data$score != "NA")
-all_data <- all_data %>%  filter(!is.na(all_data$score))
+all_data <- all_data %>%  filter(all_data$score != "NA")
+#all_data <- all_data %>%  filter(!is.na(all_data$score))
 all_data <- all_data %>%  filter(all_data$score >= 0)
 
 #(i.) to (vi.)
@@ -96,7 +95,7 @@ all_data$inspection_type <- gsub(pattern = '[[:punct:]]', replacement = '', x = 
 #Part A(c.)
 #assign maximum score for each inspection, to all observations of said inspection
 all_data <- all_data %>% 
-  group_by(id, inspection_date) %>% 
+  group_by(id, inspection_date, score) %>% 
   mutate(max_score = max(score))
 
 all_data$score <- NULL
@@ -107,6 +106,7 @@ all_data <- all_data %>% rename(score = max_score)
 check <- all_data %>%  summarise(n_distinct(score))
 
 stored_data <- all_data
+stored_data_c <- all_data
 
 #Part B
 restaurant_data <- all_data %>% 
@@ -136,42 +136,49 @@ restaurant_data <- unique(restaurant_data)
 restaurant_data_copy_2 <- restaurant_data #copy 2
 
 #Part C(b.)
-#restrict to id, score, action, and inspection_date
-all_data_restricted <- all_data %>% select(-borough, -cuisine, -inspection_year,
-                                -code, -grade, -critical, -inspection_type)
-
-require(data.table)
-features_data <- merge(all_data_restricted, 
-                       restaurant_data,
-                       all.x = TRUE, 
-                       allow.cartesian = TRUE)
-
-
-#before 2017
-his_before_2017 <- stored_data_c %>% filter(grepl('Initial', inspection_type)) %>% filter(inspection_year < 2017)
-
-his_before_2017 <- his_before_2017 %>% dplyr::select(id, inspection_date, score, borough, cuisine, inspection_year, action)
-his_before_2017 <- his_before_2017 %>% ungroup() %>% unique()
-
-his_before_2017 <- his_before_2017 %>% mutate(num_previous_low_inspections = as.numeric(his_before_2017$score < 14),
-                                              num_previous_med_inspections = as.numeric(his_before_2017$score >= 14 & his_before_2017$score < 28),
-                                              num_previous_high_inspections = as.numeric(his_before_2017$score >= 28))
-#'re-closed' and 'closed' in action column
-his_before_2017 <- his_before_2017 %>% mutate(num_previous_closings = as.numeric(grepl('closed', his_before_2017$action)))
-his_before_2017 <- his_before_2017 %>% group_by(id)  
-
-#need to  get row sum for 4 new feature columns --Ruoyu Dec.4
-
-# then use join function to join restaurant_data and his_before_2017 by id
+# left join 
+merged <- merge(restaurant_data,all_data[c('id', 'score', 'action', 'inspection_date')],
+                by = 'id', all.x = TRUE, allow.cartesian=TRUE)
+merged <- merged %>% filter(inspection_date.y < inspection_date.x) %>% distinct
+# Get summarized merged first, then left join with restaurant_data
+merged <- merged %>% group_by(id, inspection_date.x) %>%   
+  dplyr::summarise(num_previous_low_inspections= sum(score.y <14),
+                   num_previous_med_inspections= sum(score.y >=14 & score.y <28),
+                   num_previous_high_inspections= sum(score.y >=28),
+                   num_previous_closings= sum(action %in% c('closed',"re-closed"))
+  )
 
 
+#merged <- merged %>% rename('inspection_date.x' = 'inspection_date')
+merged <- merged %>% rename('inspection_date' = 'inspection_date.x')
+restaurant_data<-restaurant_data %>% merge(merged,by = c('id','inspection_date'),all.x = TRUE)
+
+restaurant_data$initial <- NULL
+restaurant_data$n <- NULL
+restaurant_data[is.na(restaurant_data)] <- 0
+
+#Part C(c.)
+cuisine_rank <- restaurant_data %>% group_by(cuisine) %>% count(cuisine)
+cuisine_50 <- head(arrange(cuisine_rank,desc(n)), n = 50)
+restaurant_data <- restaurant_data %>% merge(cuisine_50, by = 'cuisine')
+
+
+restaurant_data <- restaurant_data %>% transform(inspection_month = as.factor(inspection_month),
+                                                 inspection_day = as.factor(inspection_day),
+                                                 outcome = as.factor(outcome),
+                                                 num_previous_low_inspections = as.factor(num_previous_low_inspections),
+                                                 num_previous_med_inspections = as.factor(num_previous_med_inspections),
+                                                 num_previous_high_inspections = as.factor(num_previous_high_inspections),
+                                                 num_previous_closings = as.factor(num_previous_closings))
+
+restaurant_data <- restaurant_data %>% droplevels()
 #Part D
 
 #only use 2015 and 2016 data for training set
-train.lr <- restaurant_data %>% filter(year==2015|year==2016)
+train.lr <- restaurant_data %>% filter(inspection_year==2015|inspection_year==2016)
 
 # use 2017 data for testing set
-test.lr <- restaurant_data %>% filter(year==2017)
+test.lr <- restaurant_data %>% filter(inspection_year==2017)
 
 # train model with training set
 model.lr <- glm(outcome ~ cuisine + borough + inspection_month + inspection_day, data=train.lr, family = 'binomial')
@@ -188,24 +195,27 @@ test.pred.lr <- prediction(test.lr$predicted.probability, test.lr$outcome)
 test.pref.lr <- performance(test.pred.lr, "auc")
 cat('the auc score is ', 100*test.pref.lr@y.values[[1]], "\n") 
 
+
 #Part E
 
 #only use 2015 and 2016 data for training set
-train.rf <- restaurant_data %>% filter(year==2015|year==2016)
+train.rf <- restaurant_data %>% filter(inspection_year==2015|inspection_year==2016)
 
 # use 2017 data for testing set
-test.rf <- restaurant_data %>% filter(year==2017)
+test.rf <- restaurant_data %>% filter(inspection_year==2017)
 
 # Create a Random Forest model with default parameters
 model.rf <- randomForest(outcome ~ cuisine + borough + inspection_month + inspection_day + 
                            num_previous_low_inspections + num_previous_med_inspections + 
                            num_previous_high_inspections + num_previous_closings, 
                          data = train.rf, ntree = 1000, importance = TRUE)
+
+
 model.rf
 
 
 # testing set
-test.rf$predicted.probability <- predict(model, newdata = test.rf, type='prob') 
+test.rf$predicted.probability <- predict(model.rf, newdata = test.rf, type='prob') 
 
 # compute AUC using ROCR package
 testrf.pred <- prediction(test.rf$predicted.probability[,2], test.rf$outcome)
@@ -213,3 +223,28 @@ testrf.perf <- performance(testrf.pred, "auc")
 cat('the auc score is ', 100*testrf.perf@y.values[[1]], "\n") 
 
 
+#Part F
+
+#Logistic regression
+test_test <- test.lr %>% arrange(desc(predicted.probability))
+
+plot.data.lr <- test.lr %>% arrange(desc(predicted.probability)) %>% 
+  mutate(num_restaurant = row_number(), percent.precision = cumsum(as.numeric(outcome))/n()) %>% 
+  select(num_restaurant, percent.precision)
+
+#Randomforest
+test_test_rf <- test.rf %>% arrange(desc(predicted.probability[,2]))
+
+plot.data.rf <- test.rf %>% arrange(desc(predicted.probability)) %>% 
+  mutate(numstops = row_number(), percent.recall = cumsum(found.contraband)/sum(found.contraband),
+         stops = numstops/n()) %>% select(stops, percent.recall)
+
+theme_set(theme_bw())
+#p <- ggplot(data=plot.data, aes(x=num_restaurant, y=percent.precision)) 
+p <- ggplot()
+p <- p + geom_line(data=plot.data.lr, aes(x=num_restaurant, y=percent.precision))
+p <- p + geom_line(data=plot.data.lr, aes(x=num_restaurant, y=percent.precision))
+p <- p + scale_x_continuous('\nNumber of Restaurants', limits=c(100, 2000), breaks=c(100,200,500,1000,1500,2000), 
+                            labels=c('100','200','500','1000','1500','2000'))
+p <- p + scale_y_continuous("Percent of Initial Inspection is higher than 28", limits=c(0, 1), labels=scales::percent)
+p
